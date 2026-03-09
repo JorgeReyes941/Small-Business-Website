2cc8e222f0dd0a818cd9755aa4d914b5ed5d27fa
Jorge Reyes <jorgereyes@usf.edu>
2026-03-08 22:32:05 -0400

    Add files via upload

---
+++ cc_7.js
@@ -0,0 +1,11 @@
+let input = document.getElementById("textInput")
+let button = document.getElementById("changeBtn")
+let headline = document.getElementById("cta")
+
+button.addEventListener("click", function(){
+
+headline.textContent = input.value
+
+})
+
+// ALL of the step should be compleated 
